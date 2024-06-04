a7a618907eb7e89673dcddef0ef35b66ded94129
karmaplush <karmaplush@gmail.com>
2024-06-04 15:22:05 +0500

    `setup.py` updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="whatsapp_api_webhook_server_python_v2",
+    name="whatsapp-api-webhook-server-python-v2",
     version="0.1.0",
     description=(
         "This library helps you easily create"
@@ -31,7 +31,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
